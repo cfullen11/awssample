e4761986736006e6609ed804b2ba1f7c72edcf10
skodaice <fullen.craig@gmail.com>
2022-03-13 14:24:32 -0400

    change go to string in action to see if runs

--- main.go
+++ main.go
@@ -15,6 +15,19 @@ import (
 )
 
 func main() {
+	//Create a new key for infra admin account and set those creds
+	ctx, nk := setInfraAdminAccess()
+	setCreds(nk)
+
+	//Get a list of ECR repos and create if does not exist
+	ecrClient := getRepos(ctx)
+	pushImage(ctx, ecrClient)
+	
+	//Create ECS if it does not exist
+	createEcs(ctx)
+}
+
+func setInfraAdminAccess()(context.Context, *iam.CreateAccessKeyOutput) {
 	userName := "infra-admin"
 	maxItems := 10
 	ctx := context.Background()
@@ -24,9 +37,10 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Create an Amazon S3 service client
+	// Create and iam client using the local config of the iam-admin
 	client := iam.NewFromConfig(cfg)
 
+	//List the existing keys of the infra admin user
 	input := &iam.ListAccessKeysInput{
 		MaxItems: aws.Int32(int32(maxItems)),
 		UserName: &userName,
@@ -37,6 +51,7 @@ func main() {
 		log.Fatal(err)
 	}
 
+	//For each existing key make inactive and then create a new key to be used for this session
 	for _, key := range ak.AccessKeyMetadata {
 		if key.Status == "Inactive" {
 			_, err := client.DeleteAccessKey(ctx, &iam.DeleteAccessKeyInput{
@@ -69,9 +84,7 @@ func main() {
 	// 	AccessKeyId: nk.AccessKey.AccessKeyId,
 	// 	Status: "Inactive",
 	// })
-	setCreds(nk)
-	getRepos(ctx)
-	createEcs(ctx)
+	return ctx, nk
 }
 
 func setCreds(nk *iam.CreateAccessKeyOutput) {
@@ -103,7 +116,7 @@ func getEcsClient() *ecs.Client{
 	return svcClient
 }
 
-func getRepos(ctx context.Context){
+func getRepos(ctx context.Context)*ecr.Client{
 	client := getEcrClient()
 	repoName := "skodaice"
 	//var repos = []string {repoName}
@@ -115,12 +128,12 @@ func getRepos(ctx context.Context){
 	if len(repodata.Repositories) == 1 {
 		fmt.Printf("Repository %s already exists skipping create", *repodata.Repositories[0].RepositoryName)
 	}else{
-		createEcr(ctx, &repoName)
+		createEcr(ctx, &repoName, client)
 	}
+	return client
 }
 
-func createEcr(ctx context.Context, repoName *string){
-	client := getEcrClient()
+func createEcr(ctx context.Context, repoName *string, client *ecr.Client){
 	output, err := client.CreateRepository(ctx, &ecr.CreateRepositoryInput{
 		RepositoryName: repoName,
 	})
@@ -128,6 +141,22 @@ func createEcr(ctx context.Context, repoName *string){
 		log.Fatal(err)
 	}
 	fmt.Printf("Repo %s created", *output.Repository.RepositoryName)
+}
+
+func pushImage(ctx context.Context, client *ecr.Client){
+	manifest := `locationName:"dockerfile" min:"1" type:"string" required:"true"`
+	imageTag := "latest"
+	repoName := "skodaice"
+	input := &ecr.PutImageInput{
+		ImageManifest: &manifest,
+		ImageTag: &imageTag,
+		RepositoryName: &repoName,
+	}
+	output, err := client.PutImage(ctx, input)
+	if err != nil {
+		fmt.Println("Error pushing image manifest" + err.Error())
+	}
+	fmt.Println(output.Image.ImageId)
 }
 
 func createEcs(ctx context.Context){
@@ -141,5 +170,5 @@ func createEcs(ctx context.Context){
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(output.Cluster.ClusterName)
+	fmt.Println(*output.Cluster.ClusterName)
 }
